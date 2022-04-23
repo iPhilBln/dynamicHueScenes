064b523224ca89bac9d4afbf65162bfced04ca6f
iPhil <philippmielke@icloud.com>
2022-04-24 01:12:11 +0200

    Backup-, Commands-, Change Ordner werden gelöscht - fertig implementiert

--- Sourcecode/createStates.js
+++ Sourcecode/createStates.js
@@ -1,8 +1,9 @@
 //Entfernt 0_userdata.0. aus der directory, falls sie fälschlicherweise mit angegeben wurde
 if (directory.indexOf('0_userdata.0.' ) + 1 > 0) directory = directory.replace('0_userdata.0.', '');
 
-settingsDirectory = '0_userdata.0.' + directory + '.0_Settings.' + Group_ID
-groupDirectory = '0_userdata.0.' + directory + Group_ID
+settingsDirectory = '0_userdata.0.' + directory + '.0_Settings.' + Group_ID;
+groupDirectory = '0_userdata.0.' + directory + Group_ID;
+selectorDirectory = 'state[id=*](dynamic_lightscenes=' + Group_ID + ')';
 
 //legt Datenpunkt für minimale Duration an, falls er nicht existiert
 //sonst werden Standardwerte hinzugefügt
@@ -11,9 +12,9 @@ createState(settingsDirectory + '.Duration.duration_min', 0, true, {
   'read': true,
   'write': true,
   'type': 'number'
-}, function(err){
-  if (!err) console.log("Der Datenpunkt für die minimale Dauer des Farbwechsels wurde angelegt.");
-  else console.log("Der Datenpunkt die minimale Dauer konnte nicht erstellt werden: " + err);
+  }, function(err){
+        if (!err) console.log("Der Datenpunkt für die minimale Dauer des Farbwechsels wurde angelegt.");
+        else console.log("Der Datenpunkt für die minimale Dauer konnte nicht erstellt werden: " + err);
 });
 
 //legt Datenpunkt für maximale Duration an, falls er nicht existiert
@@ -23,9 +24,9 @@ createState(settingsDirectory + '.Duration.duration_max', 0, true, {
   'read': true,
   'write': true,
   'type': 'number'
-}, function(err){
-  if (!err) console.log("Der Datenpunkt für die maximale Dauer des Farbwechsels wurde angelegt.");
-  else console.log("Der Datenpunkt die maximale Dauer konnte nicht erstellt werden: " + err);
+  }, function(err){
+        if (!err) console.log("Der Datenpunkt für die maximale Dauer des Farbwechsels wurde angelegt.");
+        else console.log("Der Datenpunkt für die maximale Dauer konnte nicht erstellt werden: " + err);
 });
 
 //legt Datenpunkt für minimale Brightness an, falls er nicht existiert
@@ -35,11 +36,10 @@ createState(settingsDirectory + '.Brightness.brightness_min', 0, true, {
     'read': true,
     'write': true,
     'type': 'number'
-}, function(err){
-  if (!err) console.log("Der Datenpunkt für die minimale Helligkeit der Lampengruppe wurde angelegt.");
-  else console.log("Der Datenpunkt die minimale Helligkeit konnte nicht erstellt werden: " + err);
+  }, function(err){
+        if (!err) console.log("Der Datenpunkt für die minimale Helligkeit der Lampengruppe wurde angelegt.");
+        else console.log("Der Datenpunkt für die minimale Helligkeit konnte nicht erstellt werden: " + err);
 });
-
 
 //legt Datenpunkt für maximale Brightness an, falls er nicht existiert
 //sonst werden Standardwerte hinzugefügt
@@ -48,32 +48,28 @@ createState(settingsDirectory + '.Brightness.brightness_max', 0, true, {
     'read': true,
     'write': true,
     'type': 'number'
-}, function(err){
-  if (!err) console.log("Der Datenpunkt für die maximale Helligkeit der Lampengruppe wurde angelegt.");
-  else console.log("Der Datenpunkt die maximale Helligkeit konnte nicht erstellt werden: " + err);
+  }, function(err){
+        if (!err) console.log("Der Datenpunkt für die maximale Helligkeit der Lampengruppe wurde angelegt.");
+        else console.log("Der Datenpunkt für die maximale Helligkeit konnte nicht erstellt werden: " + err);
 });
 
 //löscht, falls vorhanden, Backupordner
-if (existsObject(settingsDirectory + 'Backup')){
-  deleteObject(settingsDirectory + 'Backup');
-  console.log("Der Backupordner wurde erfolgreich gelöscht.")
-}
+deleteObject(settingsDirectory + '.Backup', {recursive:true}, function(err){
+  if (!err) console.log("Der Backupordner wurde erfolgreich gelöscht.");
+  else console.log("Der Backupordner konnte nicht gelöscht werden: " + err);
+});
 
 //löscht, falls vorhanden, Changeordner
-if (existsObject(settingsDirectory + 'Change')){
-  deleteObject(settingsDirectory + 'Change', function(err){
-    if (!err) console.log("Der Changeordner wurde erfolgreich gelöscht.");
-    else console.log("Der Changeordner konnte nicht gelöscht werden: " + err);
-  });
-
-}
+deleteObject(settingsDirectory + '.Change', {recursive:true}, function(err){
+  if (!err) console.log("Der Changeordner wurde erfolgreich gelöscht.");
+  else console.log("Der Changeordner konnte nicht gelöscht werden: " + err);
+});
 
 //löscht, falls vorhanden, Commandsordner
-if (existsObject(settingsDirectory + 'Commands')){
-  deleteObject(settingsDirectory + 'Commands', function(err){
-    console.log("Der Commandsordner wurde erfolgreich gelöscht.")
-  });
-}
+deleteObject(settingsDirectory + '.Commands', {recursive:true}, function(err){
+  if(!err) console.log("Der Commandsordner wurde erfolgreich gelöscht.");
+  else console.log("Der Commandsordner konnte nicht gelöscht werden: " + err);
+});
 
 
 
